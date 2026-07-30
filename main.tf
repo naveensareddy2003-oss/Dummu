1a155d1a0f0986409b49bcaf7c186198e2ec1431
sareddy naveenkumarreddy <naveensareddy2003@gmail.com>
2026-07-30 17:52:01 +0530

    Rename AWS instance resource and update key name

--- main.tf
+++ main.tf
@@ -11,10 +11,10 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "Terraform-EC2" {
   ami           = "ami-0b6d9d3d33ba97d99"  
   instance_type = "t3.micro"
-  key_name      = "your-key-pair-name"
+  key_name      = "winscp"
 
   tags = {
     Name = "Terraform-EC2"
