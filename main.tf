74d9d10902974f625d21c08aea0b8cf5c6b87ead
sareddy naveenkumarreddy <naveensareddy2003@gmail.com>
2026-07-30 17:48:50 +0530

    Fix formatting of AMI string in main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0b6d9d3d33ba97d99 "  
+  ami           = "ami-0b6d9d3d33ba97d99"  
   instance_type = "t3.micro"
   key_name      = "your-key-pair-name"
 
